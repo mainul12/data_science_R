58f9846c9abcf021680320113345718109623276
Shaikh Mainul Islam <kyokushinwingchun@gmail.com>
2017-04-18 08:45:28 +0600

    Update downloading_files.R

--- Getting and Cleaning Data/Week 1/downloading_files.R
+++ Getting and Cleaning Data/Week 1/downloading_files.R
@@ -7,21 +7,17 @@ getwd()
 setwd("C:\\Users\\aaaaaaaa\\Documents\\GitHub\\R\\Getting and Cleaning Data\\Week 1")
 
 ## checking for and creating data directory
-
 if (!file.exists("data")){
   dir.create("data")
 }
 
 ## downloading data from a website
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
-
 download.file(fileUrl, destfile = "./data/cameras.csv")
 
 ## check if the file has been downloaded
-
 list.files("./data")  ## should return "cameras.csv"
 
 
 ## recording the time of download
-
 dateDownloaded <- date()
